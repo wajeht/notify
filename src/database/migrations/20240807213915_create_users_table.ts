4ac9a6f09d3912cf28e00c0ae58d6665a943619e
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-07 16:55:52 -0500

    feat(database): Update users table migration to include additional columns

--- src/database/migrations/20240807213915_create_users_table.ts
+++ src/database/migrations/20240807213915_create_users_table.ts
@@ -1,5 +1,19 @@
 import type { Knex } from 'knex';
 
-export async function up(knex: Knex): Promise<void> {}
+export async function up(knex: Knex): Promise<void> {
+	await knex.schema.createTable('users', (table) => {
+		table.increments('id').primary();
+		table.string('email').unique().notNullable();
+		table.string('password').notNullable();
+		table.boolean('is_verified').defaultTo(false);
+		table.string('verification_token').nullable();
+		table.timestamp('verification_token_expires_at').nullable();
+		table.string('reset_password_token').nullable();
+		table.timestamp('reset_password_token_expires_at').nullable();
+		table.timestamps(true, true);
+	});
+}
 
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex): Promise<void> {
+	await knex.schema.dropTableIfExists('users');
+}

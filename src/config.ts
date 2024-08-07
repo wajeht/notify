d1d84dc289fa6caf378b7b9f623431453b10bea9
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-07 13:49:24 -0500

    build: Update package-lock.json and package.json with nodemailer dependency

--- src/config.ts
+++ src/config.ts
@@ -8,3 +8,13 @@ export const appConfig = {
   port: process.env.PORT as unknown as number || 80,
   env: process.env.ENV as unknown as Env || 'development'
 } as const
+
+export const emailConfig ={
+	host: process.env.EMAIL_HOST as string,
+	port: process.env.EMAIL_PORT as unknown as number,
+	email_alias: process.env.EMAIL_AUTH_ALIAS as unknown as string,
+	auth: {
+		user: process.env.EMAIL_AUTH_EMAIL  as unknown as string,
+		pass: process.env.EMAIL_AUTH_PASS as unknown as string,
+	},
+} as const;

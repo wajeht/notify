85153d41ccf70429ce69c7d96a27c3a37a812011
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-08 12:10:38 -0500

    style: Update content security policy directives in app.ts

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,15 @@ app.use(express.json({ limit: '1mb' }));
 
 app.use(express.urlencoded({ extended: true }));
 
-app.use(helmet());
+app.use(
+	helmet.contentSecurityPolicy({
+		directives: {
+			...helmet.contentSecurityPolicy.getDefaultDirectives(),
+			'default-src': ["'self'", 'plausible.jaw.dev'],
+			'script-src': ["'self'", "'unsafe-inline'", 'plausible.jaw.dev'],
+		},
+	}),
+);
 
 app.use(cors());
 

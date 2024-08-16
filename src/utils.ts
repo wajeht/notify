63bcad3277a06512007984f57c290a67824c87e0
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-16 15:28:43 -0500

    chore: wip

--- src/utils.ts
+++ src/utils.ts
@@ -2,146 +2,30 @@ import path from 'node:path';
 import { db } from './db/db';
 import { appConfig } from './config';
 
-export async function seedDatabase(force: boolean = false) {
-	try {
-		if (appConfig.env === 'production' || force == false) {
-			console.log('Cannot run database seeding in production environment');
-			return;
-		}
-
-		console.log('Starting database seeding...');
-
-		const config = {
-			directory: path.resolve(path.join(process.cwd(), 'dist', 'src', 'db', 'seeds')),
-		};
-
-		const [seedFiles] = await db.seed.run(config);
-
-		if (seedFiles.length === 0) {
-			console.log('No seed files found or all seeds have already been run');
-		} else {
-			const seedList = seedFiles.map((seedFile: string) => path.basename(seedFile)).join(', ');
-			console.log(`Database seeding completed for: ${seedList}`);
-		}
-	} catch (error) {
-		console.error('Error seeding database:', error);
-		throw error;
-	}
-}
-
-export async function cleanDatabase() {
-	try {
-		// Get all table names in the current schema
-		const tables = await db.raw(`
-      SELECT tablename FROM pg_tables
-      WHERE schemaname = current_schema()
-    `);
-		const tableNames = tables.rows.map((row: any) => row.tablename);
-
-		// Disable triggers
-		await db.raw('SET session_replication_role = replica');
-
-		// Truncate all tables
-		for (const tableName of tableNames) {
-			await db.raw(`TRUNCATE TABLE "${tableName}" CASCADE`);
-			console.log(`Table ${tableName} truncated`);
-		}
-
-		console.log('All tables have been cleaned');
-	} catch (error) {
-		console.error('Error cleaning database:', error);
-		throw error;
-	} finally {
-		// Re-enable triggers
-		await db.raw('SET session_replication_role = DEFAULT');
-	}
-}
-
-export async function runFreshMigration(force: boolean = false) {
-	try {
-		if (appConfig.env === 'production' || force == false) {
-			console.log('Cannot run fresh migration on production environment');
-			return;
-		}
-
-		const config = {
-			directory: path.resolve(path.join(process.cwd(), 'dist', 'src', 'db', 'migrations')),
-		};
-
-		console.log('Dropping all tables...');
-		await dropAllTables();
-
-		console.log('Running fresh migrations...');
-		const [batchNo, migrations] = await db.migrate.latest(config);
-
-		if (migrations.length === 0) {
-			console.log('No migrations were run. Database schema is up to date.');
-		} else {
-			const migrationList = migrations
-				.map((migration: any) => migration.split('_')[1].split('.')[0])
-				.join(', ');
-			console.log(`Fresh migrations completed for ${migrationList} schema`);
-			console.log(`Batch ${batchNo} run: ${migrations.length} migrations`);
-		}
-
-		const currentVersion = await db.migrate.currentVersion();
-		console.log(`Current database version: ${currentVersion}`);
-	} catch (error) {
-		console.error('Error running fresh migration:', error);
-		throw error;
-	}
-}
-
-async function dropAllTables() {
-	try {
-		await db.raw("SET session_replication_role = 'replica';");
-
-		const tables = await db.raw(`
-					SELECT tablename FROM pg_tables
-					WHERE schemaname = current_schema()
-					AND tablename != 'knex_migrations'
-					AND tablename != 'knex_migrations_lock'
-			`);
-
-		for (const row of tables.rows) {
-			const tableName = row.tablename;
-			await db.raw(`DROP TABLE IF EXISTS "${tableName}" CASCADE`);
-			console.log(`Dropped table: ${tableName}`);
-		}
-
-		console.log('All tables have been dropped.');
-
-		await db.raw("SET session_replication_role = 'origin';");
-	} catch (error) {
-		console.error('Error dropping tables:', error);
-		throw error;
-	}
-}
-
 export async function runMigrations() {
 	try {
+		if (appConfig.env !== 'production') {
+			console.log('Cannot run auto database migration on non production');
+			return;
+		}
+
 		const config = {
 			directory: path.resolve(path.join(process.cwd(), 'dist', 'src', 'db', 'migrations')),
 		};
 
-		const pendingMigrations = await db.migrate.list(config);
-
-		if (pendingMigrations[1].length === 0) {
-			console.log('No pending migrations.');
-			return;
-		}
-
-		console.log(`Running ${pendingMigrations[1].length} pending migrations...`);
+		const version = await db.migrate.currentVersion();
+		console.log(`Current database version ${version}`);
+		console.log(`Checking for database upgrades`);
 
 		const [batchNo, migrations] = await db.migrate.latest(config);
 
 		if (migrations.length === 0) {
-			console.log('No new migrations were executed.');
+			console.log('Database upgrade not required');
 		} else {
 			const migrationList = migrations
 				.map((migration: any) => migration.split('_')[1].split('.')[0])
 				.join(', ');
-			console.log(`Completed migrations: ${migrationList}`);
+			console.log(`Database upgrades completed for ${migrationList} schema`);
 			console.log(`Batch ${batchNo} run: ${migrations.length} migrations`);
 		}
 	} catch (error) {

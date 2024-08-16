d7fc5204028ad54c848ef6f9665a08de56001c1f
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-16 15:16:52 -0500

    refactor: run db migration

--- src/utils.ts
+++ src/utils.ts
@@ -120,28 +120,28 @@ async function dropAllTables() {
 
 export async function runMigrations() {
 	try {
-		if (appConfig.env !== 'production') {
-			console.log('Cannot run auto database migration on non production');
-			return;
-		}
-
 		const config = {
 			directory: path.resolve(path.join(process.cwd(), 'dist', 'src', 'db', 'migrations')),
 		};
 
-		const version = await db.migrate.currentVersion();
-		console.log(`Current database version ${version}`);
-		console.log(`Checking for database upgrades`);
+		const pendingMigrations = await db.migrate.list(config);
+
+		if (pendingMigrations[1].length === 0) {
+			console.log('No pending migrations.');
+			return;
+		}
+
+		console.log(`Running ${pendingMigrations[1].length} pending migrations...`);
 
 		const [batchNo, migrations] = await db.migrate.latest(config);
 
 		if (migrations.length === 0) {
-			console.log('Database upgrade not required');
+			console.log('No new migrations were executed.');
 		} else {
 			const migrationList = migrations
 				.map((migration: any) => migration.split('_')[1].split('.')[0])
 				.join(', ');
-			console.log(`Database upgrades completed for ${migrationList} schema`);
+			console.log(`Completed migrations: ${migrationList}`);
 			console.log(`Batch ${batchNo} run: ${migrations.length} migrations`);
 		}
 	} catch (error) {

125b25cf04bc9b088a3874a7180e35f28b072f23
wajeht <58354193+wajeht@users.noreply.github.com>
2024-08-16 15:29:18 -0500

    chore: wip

--- src/utils.ts
+++ src/utils.ts
@@ -14,20 +14,25 @@ export async function runMigrations() {
 		};
 
 		const version = await db.migrate.currentVersion();
+
 		console.log(`Current database version ${version}`);
+
 		console.log(`Checking for database upgrades`);
 
 		const [batchNo, migrations] = await db.migrate.latest(config);
 
 		if (migrations.length === 0) {
 			console.log('Database upgrade not required');
-		} else {
-			const migrationList = migrations
-				.map((migration: any) => migration.split('_')[1].split('.')[0])
-				.join(', ');
-			console.log(`Database upgrades completed for ${migrationList} schema`);
-			console.log(`Batch ${batchNo} run: ${migrations.length} migrations`);
+			return;
 		}
+
+		const migrationList = migrations
+			.map((migration: any) => migration.split('_')[1].split('.')[0])
+			.join(', ');
+
+		console.log(`Database upgrades completed for ${migrationList} schema`);
+
+		console.log(`Batch ${batchNo} run: ${migrations.length} migrations`);
 	} catch (error) {
 		console.error('Error running migrations:', error);
 		throw error;

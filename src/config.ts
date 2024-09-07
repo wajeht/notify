16ee6ac0c9d4f4bb61a0a7e64117c04f20cf0e5b
wajeht <58354193+wajeht@users.noreply.github.com>
2024-09-07 18:28:35 -0500

    feat: backblaz3e config

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import dotenv from 'dotenv';
 import { Env } from './types';
+import { S3Client } from '@aws-sdk/client-s3';
 
 dotenv.config({ path: path.resolve(path.join(process.cwd(), '.env')) });
 
@@ -57,3 +58,21 @@ export const sessionConfig = {
 	secret: process.env.SESSION_SECRET || 'notify',
 	domain: process.env.SESSION_DOMAIN || 'localhost',
 } as const;
+
+export const backBlaze = {
+	bucket: process.env.BACKBLAZE_BUCKET as unknown as string,
+	region: process.env.BACKBLAZE_REGION as unknown as string,
+	end_point: process.env.BACKBLAZE_END_POINT as unknown as string,
+	key_id: process.env.BACKBLAZE_KEY_ID as unknown as string,
+	application_key: process.env.BACKBLAZE_APPLICATION_KEY as unknown as string,
+} as const;
+
+export const s3BucketConfig = new S3Client({
+	credentials: {
+		accessKeyId: backBlaze.key_id,
+		secretAccessKey: backBlaze.application_key,
+	},
+	region: backBlaze.region,
+	forcePathStyle: true,
+	endpoint: backBlaze.end_point,
+});
